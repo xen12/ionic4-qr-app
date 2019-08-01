a4cde1c1b00a52846e1832bf0bfe040b304174ba
xen12 <gus.xen.12@gmail.com>
2019-08-01 18:52:10 -0400

    ToastController - Notificaciones en pantalla

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,6 +1,10 @@
 import { Component } from '@angular/core';
 
+// plugins
 import { BarcodeScanner } from '@ionic-native/barcode-scanner/ngx';
+
+// components
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -9,7 +13,7 @@ import { BarcodeScanner } from '@ionic-native/barcode-scanner/ngx';
 })
 export class HomePage {
 
-  constructor( private barcodeScanner:BarcodeScanner ) {}
+  constructor( private barcodeScanner:BarcodeScanner, private toastCtrl:ToastController ) {}
 
   scan() {
     console.log("Realizando scan");
@@ -18,7 +22,44 @@ export class HomePage {
       console.log('BarcodeData: ' + barcodeData);
     }).catch(err => {
       console.log('Error: ' + err);
+      this.mostrarError('Error: ' + err);
+      //this.mostrarError2();
     });
   }
+
+  async mostrarError( mensaje:string ) {
+    const toast = await this.toastCtrl.create({
+      message: mensaje,
+      position: 'top',
+      duration: 2500
+    });
+    toast.present();
+  }
+
+  async mostrarError2(  ) {
+    const toast = await this.toastCtrl.create({
+      header: 'Toast header',
+      message: 'Click to Close',
+      position: 'top',
+      buttons: [
+        {
+          side: 'start',
+          icon: 'star',
+          text: 'Favorite',
+          handler: () => {
+            console.log('Favorite clicked');
+          }
+        }, {
+          text: 'Done',
+          role: 'cancel',
+          handler: () => {
+            console.log('Cancel clicked');
+          }
+        }
+      ]
+    });
+    toast.present();
+  }
+
 
 }
